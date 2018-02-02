0239d083f81a8d8b4fa43c27d5612f0422a80cc0
niko <niko@localhost.localdomain>
2018-02-02 16:52:53 +0100

    v1.0

--- save_home.bash
+++ save_home.bash
@@ -1,3 +1,14 @@
 #! /bin/bash
 
+ARCHIVE_NAME="home_"`date +%d_%m_%Y`
+FILE_TO_SAVE=/home
+SAVE_FOLDER=/sauvegarde/travail/
+
+if [ -e $SAVE_FOLDER ] 
+then
+  tar -cvzf "$SAVE_FOLDER$ARCHIVE_NAME".tar.gz "$FILE_TO_SAVE"
+else
+  echo "can't find $SAVE_FOLDER"
+fi
+
 
